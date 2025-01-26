3650965366724779dea7be4281d6c5408d491da8
Ash Joseph <kamikazedesigns@gmail.com>
2025-01-26 14:47:43 +0000

    Added markdown helper
    added some default text formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     component = name.concat("Component").constantize
     render(component.new(*, **), &)
   end
+
+  def render_markdown(&block)
+    content = capture(&block).strip_heredoc
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
+    markdown.render(content).html_safe
+  end
 end
