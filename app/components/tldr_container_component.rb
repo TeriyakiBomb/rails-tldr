013efe7ed1828612e4aed740299691751135da13
Ash Joseph <kamikazedesigns@gmail.com>
2025-01-26 18:07:26 +0000

    Moved markdown rendering into a slot
    Article updates
    Redcarpet allows html now

--- app/components/tldr_container_component.rb
+++ app/components/tldr_container_component.rb
@@ -2,12 +2,13 @@
 
 class TldrContainerComponent < ApplicationComponent
   renders_one :tldr
+  renders_many :markdown_sections
 
   option :current_page
 
   def render_markdown(&block)
     content = capture(&block).strip_heredoc
-    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
     markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
     markdown.render(content).html_safe
   end
